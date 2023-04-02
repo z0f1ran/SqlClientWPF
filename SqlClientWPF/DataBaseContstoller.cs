98ece339e3b6a36b15905a3152c61b16d3ccc14c
top_acdemy_student <top_acdemy_student@OR-08>
2023-04-02 13:09:49 +0700

    Добавьте файлы проекта.

---
+++ SqlClientWPF/DataBaseContstoller.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Animation;
+
+namespace SqlClientWPF
+{
+    internal class DataBaseContstoller
+    {
+        public DataBaseContstoller(string connectionString)
+        {
+            ConnectionString = connectionString;
+        }
+        public DataBaseContstoller()
+        {
+            ConnectionString = null;
+        }
+
+        private string ConnectionString { get; set; }
+        public string ExecuteSql(string query)
+        {
+            SqlConnection connection = null;
+            try
+            {
+                connection = new SqlConnection(ConnectionString);
+                connection.Open();
+                SqlCommand cmd = new SqlCommand(query, connection);
+                string p = query.Split(' ')[0];
+                if (p.ToLower() == "select")
+                {
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    StringBuilder sb = new StringBuilder();
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        sb.Append(reader.GetName(i));
+                        sb.Append('-');
+                    }
+                    sb.Append("\n");
+                    while (reader.Read())
+                    {
+                        for (int i = 0; i < reader.FieldCount; i++)
+                        {
+                            sb.Append(reader[i]);
+                            sb.Append(' ');
+                        }
+                        sb.Append('\n');
+                    }
+                    return sb.ToString();
+                }
+                else if(p.ToLower() == "update" &&
+                    p.ToLower() == "insert" &&
+                    p.ToLower() == "delete")
+                {
+                    cmd.ExecuteNonQuery();
+                    return "Операция выполнена";
+                }
+                else
+                {
+                    return null;
+                }
+                
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+                return null;
+            }
+        }
+        public void SetConnectinon(string str) => ConnectionString = str; 
+    }
+}
---
+++ SqlClientWPF/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Policy;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace SqlClientWPF
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenClient(object sender, RoutedEventArgs e)
+        {
+            SqlClientMode scm = new SqlClientMode();
+            this.Visibility = Visibility.Hidden;
+            scm.ShowDialog();
+            this.Visibility = Visibility.Visible;
+        }
+
+        private void tableSqlMode_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("not done yet");
+        }
+
+        private void workWithDataMode_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("not done yet");
+        }
+
+        private void exit_Btn_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if (MessageBox.Show("You are sure?", "quest", MessageBoxButton.YesNo) != MessageBoxResult.Yes)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
